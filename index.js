b13e00a135d3feaccc53d83fbf821996e2eb95d4
Ankush Date <117070673+AnkushDate18@users.noreply.github.com>
2023-08-11 17:39:54 +0530

    second updatet

--- index.js
+++ index.js
@@ -7,3 +7,4 @@ let c =a+b+v;
 console.log("additiona of two numbers ",c);
 
 "mkcl are package are loading and are the avjhdvasah gbka fkg"
+"mkcl are package are loading and are the avjhdvasah gbka fkg"
